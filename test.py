cdef9297c17ab505d3974f124e3c58315ad9206f
rockerBOO <rockerboo@gmail.com>
2023-11-15 11:56:42 -0500

    Add range of samplers

--- test.py
+++ test.py
@@ -7,7 +7,20 @@ import sys
 import time
 
 import pandas as pd
-from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import (
+    StableDiffusionPipeline,
+    DPMSolverMultistepScheduler,
+    DDPMScheduler,
+    EulerAncestralDiscreteScheduler,
+    DPMSolverSinglestepScheduler,
+    LMSDiscreteScheduler,
+    PNDMScheduler,
+    DDIMScheduler,
+    EulerDiscreteScheduler,
+    HeunDiscreteScheduler,
+    KDPM2DiscreteScheduler,
+    KDPM2AncestralDiscreteScheduler,
+)
 from tqdm import tqdm
 import numpy as np
 import torch
@@ -35,6 +48,31 @@ model_id_map = {
     "v2-1-large": "stabilityai/stable-diffusion-2-1",
 }
 
+samplers = [
+    "ddim",
+    "pndm",
+    "lms",
+    "euler",
+    "euler_a",
+    "heun",
+    "dpm_2",
+    "dpm_2_a",
+    "dpmsolver",
+    "dpmsolver++",
+    "dpmsingle",
+    "k_lms",
+    "k_euler",
+    "k_euler_a",
+    "k_dpm_2",
+    "k_dpm_2_a",
+]
+
+# scheduler:
+SCHEDULER_LINEAR_START = 0.00085
+SCHEDULER_LINEAR_END = 0.0120
+SCHEDULER_TIMESTEPS = 1000
+SCHEDLER_SCHEDULE = "scaled_linear"
+
 
 def main(args):
     # args.lemma = cached_nlp(args.word)[0].lemma_ if args.word else None
@@ -49,32 +87,41 @@ def main(args):
     if args.output_folder is None:
         args.output_folder = "output"
 
-    # new_prompts = []
-    #
-    # if args.lemma is not None:
-    #     for prompt_id, prompt in tqdm(prompts):
-    #         if args.lemma not in prompt.lower():
-    #             continue
-    #
-    #         doc = cached_nlp(prompt)
-    #         found = False
-    #
-    #         for tok in doc:
-    #             if tok.lemma_.lower() == args.lemma and not found:
-    #                 found = True
-    #             elif (
-    #                 tok.lemma_.lower() == args.lemma
-    #             ):  # filter out prompts with multiple instances of the word
-    #                 found = False
-    #                 break
-    #
-    #         if found:
-    #             new_prompts.append((prompt_id, prompt))
-    #
-    #     prompts = new_prompts
+    sched_init_args = {}
+    if args.sampler == "ddim":
+        scheduler_cls = DDIMScheduler
+    elif args.sampler == "ddpm":  # ddpmはおかしくなるのでoptionから外してある
+        scheduler_cls = DDPMScheduler
+    elif args.sampler == "pndm":
+        scheduler_cls = PNDMScheduler
+    elif args.sampler == "lms" or args.sampler == "k_lms":
+        scheduler_cls = LMSDiscreteScheduler
+    elif args.sampler == "euler" or args.sampler == "k_euler":
+        scheduler_cls = EulerDiscreteScheduler
+    elif args.sampler == "euler_a" or args.sampler == "k_euler_a":
+        scheduler_cls = EulerAncestralDiscreteScheduler
+    elif args.sampler == "dpmsolver" or args.sampler == "dpmsolver++":
+        scheduler_cls = DPMSolverMultistepScheduler
+        sched_init_args["algorithm_type"] = args.sampler
+    elif args.sampler == "dpmsingle":
+        scheduler_cls = DPMSolverSinglestepScheduler
+    elif args.sampler == "heun":
+        scheduler_cls = HeunDiscreteScheduler
+    elif args.sampler == "dpm_2" or args.sampler == "k_dpm_2":
+        scheduler_cls = KDPM2DiscreteScheduler
+    elif args.sampler == "dpm_2_a" or args.sampler == "k_dpm_2_a":
+        scheduler_cls = KDPM2AncestralDiscreteScheduler
+    else:
+        scheduler_cls = DDIMScheduler
 
-    scheduler = DPMSolverMultistepScheduler()
-    # prompts = prompts[: args.gen_limit]
+    scheduler = scheduler_cls(
+        num_train_timesteps=SCHEDULER_TIMESTEPS,
+        beta_start=SCHEDULER_LINEAR_START,
+        beta_end=SCHEDULER_LINEAR_END,
+        beta_schedule=SCHEDLER_SCHEDULE,
+        **sched_init_args,
+    )
+
     pipe = StableDiffusionPipeline.from_pretrained(
         model_id,
         torch_dtype=torch.float16,
@@ -89,43 +136,42 @@ def main(args):
 
     with accelerator.autocast(), torch.no_grad():
         for gen_idx, (prompt_id, prompt) in enumerate(tqdm(prompts)):
-            # seed = int(time.time()) if args.random_seed else args.seed
-            # prompt = prompt.replace(",", " ,").replace(".", " .").strip()
+            seed = int(time.time()) if args.random_seed else args.seed
 
-            # if seeds and gen_idx < len(seeds):
-            #     seed = seeds[gen_idx]
-            #
-            # gen = set_seed(seed)
-            #
-            # if args.action == "cconj":
-            #     seed = int(prompt_id.split("-")[1]) + args.seed_offset
-            #     gen = set_seed(seed)
+            if seeds and gen_idx < len(seeds):
+                seed = seeds[gen_idx]
 
-            # prompt_id = str(prompt_id)
-            print(prompt)
+            gen = set_seed(seed)
+
+            prompt_id = str(prompt_id)
 
             with trace(
                 pipe,
                 # 512,
                 # 512,
-                384,
-                640,
+                args.width,
+                args.height,
                 low_memory=args.low_memory,
                 save_heads=args.save_heads,
                 load_heads=args.load_heads,
             ) as tc:
                 out = pipe(
                     prompt,
-                    width=384,
-                    height=640,
+                    width=args.width,
+                    height=args.height,
                     num_inference_steps=args.num_timesteps,
-                    # generator=gen,
+                    generator=gen,
                     callback=tc.time_callback,
                 )
+
+                img_filename = f"{prompt_id}-{prompt}.png"
+                print(f"Saving image to {img_filename}")
+                out.images[0].save(img_filename)
 
                 global_heat_map = tc.compute_global_heat_map(prompt=prompt)
                 for word in args.words.split(","):
                     heat_map = global_heat_map.compute_word_heat_map(word)
+                    print(f"Saving daam heatmap to {img_filename}")
                     heat_map.plot_overlay(out.images[0], out_file=f"{word}-daam.png")
 
 
@@ -154,12 +200,22 @@ if __name__ == "__main__":
     )
     parser.add_argument("--seed-offset", type=int, default=0)
     parser.add_argument("--num-timesteps", "-n", type=int, default=15)
+    parser.add_argument("--width", type=int, default=512)
+    parser.add_argument("--height", type=int, default=512)
     parser.add_argument("--all-heads", action="store_true")
     parser.add_argument("--words", type=str)
     parser.add_argument("--random-seed", action="store_true")
     parser.add_argument("--truth-only", action="store_true")
     parser.add_argument("--save-heads", action="store_true")
     parser.add_argument("--load-heads", action="store_true")
+
+    parser.add_argument(
+        "--sampler",
+        type=str,
+        default="ddim",
+        choices=samplers,
+        help=f"sampler (scheduler) type for images / サンプル出力時のサンプラー（スケジューラ）の種類",
+    )
     args = parser.parse_args()
 
     main(args)
